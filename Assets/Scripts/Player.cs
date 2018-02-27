384e8b7d68899e8852b7a56ab2344a1b57b9ca69
David Blanco Cortiñas <idemnias@gmail.com>
2018-02-27 09:03:54 +0100

    Primera subida

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    private Vector2 direction = Vector2.zero;
+    private Rigidbody2D rigidbodyPlayer;
+    private Animator animatorPlayer;
+    [SerializeField]
+    private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField]
+    private Vector2 jumpForce;
+
+    void Start () {
+        rigidbodyPlayer = GetComponent<Rigidbody2D>();
+        animatorPlayer = GetComponent<Animator>();
+	}
+	
+	
+	private void FixedUpdate () {
+        direction.x = Input.GetAxisRaw("Horizontal");
+
+        animatorPlayer.SetInteger("x", (int)direction.x);
+        direction.Normalize();
+
+        rigidbodyPlayer.velocity = direction * speed;
+        if (Input.GetKeyDown(jumpKey))
+        {
+            rigidbodyPlayer.AddForce(new Vector2(0, 10));
+
+        }
+
+
+    }
+}
